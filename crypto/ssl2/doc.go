ad85de03d30bdccbdc993d6abeae98ffeffc863d
HD Moore <x@hdm.io>
2026-04-25 16:01:00 -0500

    ssl2: remove duplicated package headers

--- crypto/ssl2/doc.go
+++ crypto/ssl2/doc.go
@@ -1,4 +1,3 @@
-package ssl2
 // Copyright 2026 The runZero contributors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
